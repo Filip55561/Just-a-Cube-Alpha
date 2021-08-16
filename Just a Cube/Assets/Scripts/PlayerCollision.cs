60650e73a3981f8cc65cc7120528286154889646
Filip55561 <88946851+Filip55561@users.noreply.github.com>
2021-08-16 15:32:47 +0200

    Just a Cube Alpha

---
+++ Just a Cube/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+        // This Function runs when we hit another object.
+        // We Get information about the collision and call it "collisionInfo"
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        // We check if the object we collided with has tag called "Obstacle".
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
